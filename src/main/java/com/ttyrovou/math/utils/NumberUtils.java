dc837bcbe6a47e3d9c20cc6ec74c5eefb766c919
Thodoris1999 <teotyrov@gmail.com>
2018-07-05 22:30:23 +0300

    fix typo

--- src/main/java/com/ttyrovou/math/utils/NumberUtils.java
+++ src/main/java/com/ttyrovou/math/utils/NumberUtils.java
@@ -3,8 +3,10 @@ package com.ttyrovou.math.utils;
 import java.math.BigInteger;
 
 public class NumberUtils {
-    public static BigInteger gcd(BigInteger a, BigInteger b){
-        while (b.equals(BigInteger.ZERO)) {
+    public static BigInteger gcd(BigInteger a, BigInteger b) {
+        b = b.abs();
+        a = a.abs();
+        while (!b.equals(BigInteger.ZERO)) {
             BigInteger t = b;
             b = a.mod(b);
             a = t;
